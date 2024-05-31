cd8f6baeff039ab0aa44e2e5bdbbfe9fdf099c37
DineshT43 <emaildinesht43@gmail.com>
2024-05-31 13:25:38 +0530

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import './App.css';
-import { Home } from "./pages/index"
+import { Home, Login, Quiz, Result } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider, QuizProvider } from "./context/index";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <QuizProvider>
+          <App />
+        </QuizProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
